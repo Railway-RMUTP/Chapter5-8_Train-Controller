caa2900ec15864d327119de6a7a16f9212965ae1
Railway-RMUTP <railwayrmutp@gmail.com>
2020-05-28 17:43:39 +0700

    first commit

---
+++ VR train/Assets/Scenes/EngineController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineController : MonoBehaviour
+{
+    public Animator cubeAni;
+    public bool EngineOn;
+
+    public void SetEngine() {
+        if (cubeAni == null)
+            return;
+        EngineOn = !EngineOn;
+    }
+
+    void FixedUpdate () {
+        if (EngineOn) {
+            cubeAni.Play ("EngineOn");
+        } else
+            cubeAni.Play ("EngineOff");
+    }
+
+    private void Start () {
+        cubeAni.GetComponent<Animator> ();
+    }
+}
